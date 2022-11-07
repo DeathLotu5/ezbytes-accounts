dda24b3355550508a85c0ec8e277992d42fdc7ef
duongph <duongph@smartosc.com>
2022-11-07 07:45:02 +0700

    [Add] [Refresh scope] add annotation refresh scope

--- src/main/java/com/ezbytes/hellowordservice/HellowordserviceApplication.java
+++ src/main/java/com/ezbytes/hellowordservice/HellowordserviceApplication.java
@@ -2,8 +2,10 @@ package com.ezbytes.hellowordservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
+@RefreshScope
 public class HellowordserviceApplication {
 
 	public static void main(String[] args) {
